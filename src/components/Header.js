4136237eeb3d6437bfab796bf928c8968e8cfe80
chamomiletay <chamomile.tay@gmail.com>
2022-08-04 17:33:14 -0400

    Updated header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 //---- imports ----
 import React from 'react'
-import logo from './images/logo.png'
+import logo from './images/logo2.PNG'
 import { Link } from 'react-router-dom'
 import './main.css'
 
