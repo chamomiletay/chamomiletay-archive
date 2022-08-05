2d8903768892b5bc5af15ac488f7ee752ea3e24d
chamomiletay <chamomile.tay@gmail.com>
2022-08-04 22:55:52 -0400

    Created Home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
       <div>
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Spline from './Spline'
+
+const Home = () => {
+  return (
+    <div className='px-20 font-nanum-gothic'>
+
+        <div className='p-10 m-auto flex-auto items-center justify-center border-2 border-rose-200 bg-rose-50 shadow-md rounded-xl'>
+            <Spline/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
