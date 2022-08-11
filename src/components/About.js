26b2d6d288734cfc3947c16940431f7be9084f0e
chamomiletay <chamomile.tay@gmail.com>
2022-08-11 16:37:43 -0400

    Updated About component programming languages

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@
 import React from 'react'
 import tayIcon from './images/taylor_icon.png'
 import linkedInLogo from './images/linkedin-logo.svg'
+import htmlIcon from './images/html-icon.png'
+import jsIcon from './images/js-icon.png'
+import cssIcon from './images/css-icon.png'
 import './main.css'
 
 
@@ -22,13 +25,13 @@ const About = () => {
 
           <h3 className='pt-4 pb-8 text-2xl font-semibold font-space-mono text-orange-100 drop-shadow-md'>&#123; <span className='text-white italic drop-shadow-md'>Hello!</span> &#125;</h3>
               {/* <p className='pb-4 text-lg font-m-plus-rounded font-semibold italic text-slate-400'>湖にちわ。</p> */}
-          <p className='px-14 py-2 text-teal-600'>
+          <p className='px-8 py-2 text-teal-600'>
                 " I'm a detail-oriented front-end engineer and designer that is passionate about cozy aesthetics. I take pride in quickly adapting to new environments, engaging in storytelling, and ideating warm and inviting interactive spaces while engaging my audience. My drive to curate more inclusive and accessible spaces is evident in every application I build. "
           </p>
 
-          <a href='https://www.linkedin.com/in/tay-lee/' target="_blank" rel="noopener noreferrer">
+          {/* <a href='https://www.linkedin.com/in/tay-lee/' target="_blank" rel="noopener noreferrer">
             <img src={linkedInLogo} alt='LinkedIn logo'></img>
-          </a>
+          </a> */}
           
         </div>
       </div>
@@ -40,10 +43,6 @@ const About = () => {
       <div className='pb-8 px-4 mx-6 font-space-mono text-sm font-semibold text-sky-900'>
 
         <h4 className='pb-6 text-lg font-semibold font-space-mono text-orange-100'>&#123; <span className='text-white italic drop-shadow-md'>Programming Languages</span> &#125;</h4>
-
-        <p className='p-2 mx-2 border-2 border-blue-300 bg-blue-100 shadow-md rounded-xl w-1/6 float-left'>
-          React
-        </p>
               
         <p className='p-2 mx-2 border-2 border-blue-300 bg-blue-100 shadow-md rounded-xl w-1/6 float-left'>
           Javascript
