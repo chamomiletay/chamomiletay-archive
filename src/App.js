8ff5aa22865f2f4fdcf7199294c9b5f8316ecfea
chamomiletay <chamomile.tay@gmail.com>
2022-08-11 23:59:14 -0400

    Added page navigation

--- src/App.js
+++ src/App.js
@@ -15,15 +15,28 @@ function App() {
 
       <Header />
 
-      <About />
+      <nav className='font-nanum-gothic text-slate-500 font-semibold text-md pb-10'>
+        <a href='#about' className='hover:underline'>About </a>|
+        <a href='#projects' className='hover:underline'> Dev Projects </a>|
+        <a href='#contact' className='hover:underline'> Contact</a>
+      </nav>
+
+      <section id='about'>
+        <About />
+      </section>
 
         <div className='m-auto my-10 border-2 border-slate-300 rounded-2xl h-6 w-6 flex items-center justify-center bg-slate-100 animate-pulse'></div>
 
+    <section id='projects'>
+    <h3 className='pt-4 pb-8 text-2xl font-semibold font-space-mono text-orange-100 drop-shadow-md'>&#123; <span className='text-slate-500 italic drop-shadow-md'>Projects</span> &#125;</h3>
       <Projects />
+    </section>
 
         <div className='m-auto my-10 border-2 border-slate-300 rounded-2xl h-6 w-6 flex items-center justify-center bg-slate-100 animate-pulse'></div>
 
+    <section id='contact'>
       <Contact />
+    </section>
 
       <Footer />
 
