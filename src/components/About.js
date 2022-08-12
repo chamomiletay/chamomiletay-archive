9f28a414980d3148cb7f7c399d267697baa0fa12
chamomiletay <chamomile.tay@gmail.com>
2022-08-12 08:13:04 -0400

    Updated About component description and styling

--- src/components/About.js
+++ src/components/About.js
@@ -20,8 +20,8 @@ const About = () => {
 
           <h3 className='pt-4 pb-8 text-2xl font-semibold font-space-mono text-orange-100 drop-shadow-md'>&#123; <span className='text-white italic drop-shadow-md'>Hello!</span> &#125;</h3>
               {/* <p className='pb-4 text-lg font-m-plus-rounded font-semibold italic text-slate-400'>湖にちわ。</p> */}
-          <p className='px-8 py-2 text-teal-700'>
-                " My name is Taylor, and I have a passion for bright colors and storytelling. I'm a detail-oriented front-end engineer and designer that is passionate about cozy aesthetics.
+          <p className='px-8 py-2 text-teal-700 font-medium'>
+                " My name is Taylor, and I am passionate about storytelling. As a detail-oriented front-end engineer and designer, I truly enjoy curating responsive, efficient, and cozy spaces. My toolkit includes <span className='font-roboto font-semibold text-teal-800'>Javascript ReactJS, Tailwind CSS,</span> and other libraries and frameworks.
                 <br/> 
                 <br/> 
                 I take pride in quickly adapting to new environments, engaging in storytelling, and ideating warm and inviting interactive spaces while engaging my audience. My drive to curate more inclusive and accessible spaces is evident in every application I build. "
